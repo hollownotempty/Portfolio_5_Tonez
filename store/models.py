720e48932e0c0e886d0805b47deb93dfb1242fd2
Tom Holohan <thomasholohan8@gmail.com>
2022-02-25 15:09:34 +0000

    Deleted files from directories

--- store/models.py
+++ store/models.py
@@ -4,10 +4,11 @@ from django.db import models
 
 
 class Categories(models.Model):
-    category_name = models.CharField(max_length=120)
+    name = models.CharField(max_length=120)
+    friendly_name = models.CharField(max_length=120)
 
     def __str__(self):
-        return self.category_name
+        return self.friendly_name
 
 class Packs(models.Model):
     name = models.CharField(max_length=100)
--- store/views.py
+++ store/views.py
@@ -1,15 +1,32 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.db.models import Q
 from .models import Packs
 # Create your views here.
 
 
 def packs(request):
     # Return packs page
-
     packs = Packs.objects.all()
+    query = None
+    category = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category']
+            packs = packs.filter(category__name=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please enter a search!")
+                return redirect(reverse('packs'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            packs = packs.filter(queries)
 
     context = {
         'packs': packs,
+        'search_term': query,
     }
 
     return render(request, 'store/packs.html', context)
