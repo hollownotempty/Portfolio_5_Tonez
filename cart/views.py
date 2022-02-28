e3f1c06129cc308019d80b53eb0c56dde91374ac
Tom Holohan <thomasholohan8@gmail.com>
2022-02-28 12:25:30 +0000

    Add 'remove from bag' functionality

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
 # Create your views here.
@@ -24,3 +24,14 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def remove_from_cart(request, item_id):
+    """ Add items to the bag """
+
+    cart = request.session.get('cart', {})
+
+    cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('shopping_cart'))
