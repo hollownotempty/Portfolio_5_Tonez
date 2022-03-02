38d8d1f6eb69c353c30f8c9f06f69deeafdc3ca5
Tom Holohan <thomasholohan8@gmail.com>
2022-03-02 13:59:36 +0000

    Create order form and signals

---
+++ checkout/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = order
+        fields = (
+            'full_name', 'email', 'phone_number',
+        )
+
+    def __init__(self. *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = (
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+        )
+
+        self.fields['full_name'].widget.attrs['autofocus']= True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
