3e355bbaa32524f631ff95cc740248b5111a4348
Tom Holohan <thomasholohan8@gmail.com>
2022-03-05 15:22:27 +0000

    Install Stripe

--- Tonez/settings.py
+++ Tonez/settings.py
@@ -178,3 +178,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY")
+STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
+STRIPE_WEBHOOK_SECRET = ""
