b2cf1960e8821cdd20579987b9ab51c01bafcdaf
Tom Holohan <81058831+hollownotempty@users.noreply.github.com>
2022-03-08 16:28:28 +0000

    Added discount code capability to Stripe checkout

--- checkout/views.py
+++ checkout/views.py
@@ -78,6 +78,7 @@ def create_checkout_session(request):
     checkout_session = stripe.checkout.Session.create(
         line_items=line_items,
         mode='payment',
+        allow_promotion_codes=True,
         success_url=YOUR_DOMAIN + 'checkout/success/',
         cancel_url=YOUR_DOMAIN + 'checkout/cancel/',
     )
