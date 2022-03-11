ae24029aa1be62b00958178c2862cfe5ceea9b62
Tom Holohan <81058831+hollownotempty@users.noreply.github.com>
2022-03-11 13:55:30 +0000

    Revert "Set up login and logout, load allauth templates"
    This reverts commit 5c62db39f4d2a05c595f83cb07bfe2e6d6f83873.

--- profiles/models.py
+++ profiles/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 # Create your models here.
 class UserProfile(models.Model):
@@ -12,13 +10,3 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
-
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    """
-    Create or update user profile
-    """
-    if created:
-        UserProfile.objects.create(user=instance)
-    instance.userprofile.save()
--- profiles/views.py
+++ profiles/views.py
@@ -1,17 +1,13 @@
-from django.shortcuts import render, get_object_or_404
-
-from .models import UserProfile
+from django.shortcuts import render
 
 # Create your views here.
 
-def profile_page(request):
+def profile(request):
     """
     Display the currently logged in user's profile
     """
-    profile = get_object_or_404(UserProfile, user=request.user)
     template = 'profiles/profile.html'
     context = {
-        'profile': profile
     }
 
     return render(request, template, context)
