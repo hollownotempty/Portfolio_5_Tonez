5c62db39f4d2a05c595f83cb07bfe2e6d6f83873
Tom Holohan <81058831+hollownotempty@users.noreply.github.com>
2022-03-11 12:06:46 +0000

    Set up login and logout, load allauth templates

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
 
 # Create your views here.
 
-def profile(request):
+def profile_page(request):
     """
     Display the currently logged in user's profile
     """
+    profile = get_object_or_404(UserProfile, user=request.user)
     template = 'profiles/profile.html'
     context = {
+        'profile': profile
     }
 
     return render(request, template, context)
