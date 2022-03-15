8855724da159956928481dffd0d0d8f9dee73a92
Tom Holohan <81058831+hollownotempty@users.noreply.github.com>
2022-03-15 13:34:33 +0000

    Added toasts and css changes

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import get_object_or_404, render, redirect, reverse
 from django.contrib import messages
+from store.models import Packs
 
 # Create your views here.
 
@@ -13,6 +14,8 @@ def shopping_cart(request):
 def add_to_cart(request, item_id):
     """ Add items to the bag """
 
+    product = Packs.objects.get(pk=item_id)
+
     quantity = 1
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -20,6 +23,7 @@ def add_to_cart(request, item_id):
     if item_id in list(cart.keys()):
         messages.warning(request, 'You already have this pack in your cart!')
     else:
+        messages.success(request, f'Added {product.name} to your cart.')
         cart[item_id] = quantity
 
     request.session['cart'] = cart
@@ -29,9 +33,13 @@ def add_to_cart(request, item_id):
 def remove_from_cart(request, item_id):
     """ Add items to the bag """
 
+    product = get_object_or_404(Packs, pk=item_id)
+
     cart = request.session.get('cart', {})
 
     cart.pop(item_id)
 
     request.session['cart'] = cart
+
+    messages.success(request, f'Removed {product.name} from your cart.')
     return redirect(reverse('shopping_cart'))
